bb34b666522c1f5d626990a0e4f2993c6012107b
Ryzh <gooddmitriy@mail.ru>
2023-12-08 12:42:26 +0300

    Main window parts
    Menu and database picker

--- grid_entities/good.gd
+++ grid_entities/good.gd
@@ -5,13 +5,14 @@ var good_id: String
 var good_name: String
 var priority: String
 
-## Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-#	$HBoxContainer/Id.text = good_id
-#	$HBoxContainer/Name.text = good_name
-#	$HBoxContainer/Priority.text = priority
-#
-#func _init(_good_id: String = "0", _good_name: String = "noname", _priority: String = "null") -> void:
-#	good_id = _good_id
-#	good_name = _good_name
-#	priority = _priority
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$HBoxContainer/Id.text = good_id
+	$HBoxContainer/Name.text = good_name
+	$HBoxContainer/Priority.text = priority
+
+func _init(_good_id: String = "0", _good_name: String = "noname", 
+	_priority: String = "null") -> void:
+	good_id = _good_id
+	good_name = _good_name
+	priority = _priority
