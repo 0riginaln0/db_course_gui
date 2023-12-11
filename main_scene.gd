3cfb03894c38f5c9848fdbe0c9a2ebf8b0306dad
Ryzh <gooddmitriy@mail.ru>
2023-12-12 02:59:07 +0300

    Final countdown

--- main_scene.gd
+++ main_scene.gd
@@ -10,14 +10,12 @@ func _on_timer_timeout() -> void:
 
 func _on_reports_button_up() -> void:
 	$Journals.hide()
-	$Directory.hide()
 	$Reports.show()
 	pass # eplace with function body.
 
 
 func _on_journal_button_up() -> void:
 	$Reports.hide()
-	$Directory.hide()
 	$Journals.show()
 	pass # Replace with function body.
 
@@ -25,5 +23,4 @@ func _on_journal_button_up() -> void:
 func _on_directory_button_up() -> void:
 	$Journals.hide()
 	$Reports.hide()
-	$Directory.show()
 	pass # Replace with function body.
